94a403655fc8d1a90e36e51c5211e0bc9050d11f
Muhammadmarch09 <muhammadmarch@gmail.com>
2024-07-12 21:26:30 +0600

    making something dynamic

---
+++ Library.js
@@ -0,0 +1,7 @@
+// Making dynamic the buttons// 
+let avaiButtons = document.getElementById("avai");
+let listOfBooks = document.getElementById("book");
+
+avaiButtons.addEventListener("click", function() {
+    listOfBooks.style.display = "block";
+}); // Corrected the syntax error
